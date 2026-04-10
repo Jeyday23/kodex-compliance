32d9b809f5ca0c36337c8570280ce2af6b4f4a13
Jeremiah <jeymanagment3040@gmail.com>
2026-04-10 12:02:40 +0200

    purge: remove VibeCrush start script

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/env bash
-set -e
-source .venv/bin/activate
-echo "Migrating DB..."
-PYTHONPATH=. alembic upgrade head
-echo "Starting FastAPI on :8000..."
-PYTHONPATH=. uvicorn app.main:app --reload --host 0.0.0.0 --port 8000
+echo "DEPRECATED - Use npm run dev"
